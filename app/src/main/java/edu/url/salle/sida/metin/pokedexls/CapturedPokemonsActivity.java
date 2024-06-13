22023d913ad3904e535c538ac5ea5a5dac1e1561
sida <sidametin@gmail.com>
2024-06-13 13:54:27 +0200

    Back buttons added

--- app/src/main/java/edu/url/salle/sida/metin/pokedexls/CapturedPokemonsActivity.java
+++ app/src/main/java/edu/url/salle/sida/metin/pokedexls/CapturedPokemonsActivity.java
@@ -2,7 +2,9 @@ package edu.url.salle.sida.metin.pokedexls;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,20 +21,25 @@ public class CapturedPokemonsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_captured_pokemons);
 
+        ImageButton backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
 
         capturedPokemonsListView = findViewById(R.id.captured_pokemons_list);
 
-        // Get the captured Pokemon details from shared preferences
         SharedPreferences sharedPreferences = getSharedPreferences("CapturedPokemons", MODE_PRIVATE);
         Map<String, ?> capturedPokemons = sharedPreferences.getAll();
 
-        // Convert the captured Pokemon details to a list
         ArrayList<String> capturedPokemonsList = new ArrayList<>();
         for (Object value : capturedPokemons.values()) {
             capturedPokemonsList.add(String.valueOf(value));
         }
 
-        // Display the captured Pokemon details in the list view
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, capturedPokemonsList);
         capturedPokemonsListView.setAdapter(adapter);
     }
--- app/src/main/java/edu/url/salle/sida/metin/pokedexls/ItemsActivity.java
+++ app/src/main/java/edu/url/salle/sida/metin/pokedexls/ItemsActivity.java
@@ -4,6 +4,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -25,6 +26,14 @@ public class ItemsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items);
+
+        ImageButton backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         SharedPreferences sharedPreferences = getSharedPreferences("SelectedItems", MODE_PRIVATE);
         String selectedBall = sharedPreferences.getString("selectedBall", "No ball selected");
